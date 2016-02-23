7dbc1a3f06967b8562eb6de4f5583128db6a8ccf
Siddharth Singh <siddharthsingh@iitj.ac.in>
2016-02-24 00:02:53 +0530

    fb

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -25,6 +25,7 @@ if (typeof jQuery === "undefined") { throw new Error("Bootstrap requires jQuery"
  * limitations under the License.
  * ======================================================================== */
 
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 
 +function ($) { "use strict";
 
